a3ed4e2b4f150743ddf8b0c30759b1acafdca1de
Akash123 <Akash@123>
2023-04-29 18:46:16 +0530

    first commit

---
+++ S1-SpringIOC/src/main/java/com/demo/Testing.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		Resource r=new ClassPathResource("cfg.xml");
+		BeanFactory bf=new XmlBeanFactory(r);
+		Student s=(Student)bf.getBean("std");
+		System.out.println(s);
+
+	}
+
+}
